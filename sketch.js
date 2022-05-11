aede03c679a672d5eac8143c0903b45cbb5aa7c1
Hanbee17 <hanbee.cho@sjsu.edu>
2022-05-11 15:56:07 -0700

    coding challenge

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+let cvn;
+
+function preload() {
+  myFont = loadFont('font1.otf');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  cvn = createCanvas(600, 400);
+  cvn.parent('canvas');
 }
 
 function draw() {
-  background(220);
+  background('#D4A373');
+  fill('#FEFAE0');
+  textAlign(CENTER);
+  textFont(myFont);
+  textSize(36);
+  text('ART 101 LAST PROJECT', 300, 200);
 }
